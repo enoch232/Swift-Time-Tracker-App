50593d513f70738219fab7ed6b2d52411ee5f589
Enoch Ko <enoch232@gmail.com>
2017-02-03 22:22:07 -0700

    connect components

---
+++ TimeTrackerApp/Day.swift
@@ -0,0 +1,9 @@
+//
+//  Day.swift
+//  TimeTrackerApp
+//
+//  Created by Enoch Ko on 2/3/17.
+//  Copyright © 2017 asu. All rights reserved.
+//
+
+import Foundatio
--- TimeTrackerApp/ViewController.swift
+++ TimeTrackerApp/ViewController.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var studyTextfield: UITextField!
+    @IBOutlet weak var socializingTextfield: UITextField!
+    @IBOutlet weak var electronicsTextfield: UITextField!
+    @IBOutlet weak var sleepingTextfield: UITextField!
+    @IBOutlet weak var daysLabel: UILabel!
+    @IBOutlet weak var studyingLabel: UILabel!
+    @IBOutlet weak var socializingLabel: UILabel!
+    @IBOutlet weak var electronicsLabel: UILabel!
+    @IBOutlet weak var sleepingLabel: UILabel!
+    @IBOutlet weak var averageStudyingLabel: UILabel!
+    @IBOutlet weak var averageSocializingLabel: UILabel!
+    @IBOutlet weak var averageElectronicsLabel: UILabel!
+    @IBOutlet weak var averageSleepingLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +32,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func saveButton(_ sender: UIButton) {
+    }
 
+    @IBAction func clearButton(_ sender: UIButton) {
+    }
 
 }
 
